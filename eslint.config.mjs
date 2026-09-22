c6863f3b1f2e1db1803375574b4df88007f9659f
Aum-Mangal <aummangal307@gmail.com>
2026-09-22 22:55:27 +0530

    fix(deploy): add Vite base relative path, ESLint 9 config, favicon asset resolution, and GitHub Pages workflow

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['packages/*/src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-console': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
